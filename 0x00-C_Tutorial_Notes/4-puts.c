fd24b9999086aa9f947e928ac1e8f57087143331
Edenian Knight <solomon.okomowho@gmail.com>
2024-08-09 19:22:56 +0100

    created a new file so send it

--- 0x00-C_Tutorial_Notes/4-puts.c
+++ 0x00-C_Tutorial_Notes/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+  * main - Entry point
+  * Description- a program that
+  * prints a line of text
+  * Return: Always 0 (sucess)
+  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
---
+++ 0x00-C_Tutorial_Notes/git-it.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git add .
+git commit -m "created a new file so send it"
+git push
